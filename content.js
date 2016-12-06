3cc9cc43d60704a394c03b3439e405205236be9a
asterisk37n <asterisk37n@gmail.com>
2016-12-06 14:19:28 +0900

    changed for new UI of bookmeter.com

--- content.js
+++ content.js
@@ -1,9 +1,10 @@
 window.onload = function() {
   console.log("content script loaded window", document.location.toString());
-  var inn = document.getElementById("main_right").getElementsByClassName("inner")[1].children[1];
+//  var inn = document.getElementById("main_right").getElementsByClassName("inner")[1].children[1]; for older ui
+  var action__items = document.getElementsByClassName('action__items')[0];
   var t = document.createTextNode("Ohayooo");
   var isbn = document.location.href.split('/')[4];
-  inn.appendChild(t);
+  action__items.appendChild(t);
   console.log(isbn);
   sendLinksToBackground(isbn);
 };
