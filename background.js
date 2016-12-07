2ac73294efad3c198fd85e8efc3f25885ad2d802
asterisk37n <asterisk37n@gmail.com>
2016-12-07 17:52:08 +0900

    trying request but fails

--- background.js
+++ background.js
@@ -3,25 +3,42 @@
 // found in the LICENSE file.
 
 console.log('Bckground script started');
+var isbn;
+var systemid_list;
+var pref_name;
 chrome.tabs.onUpdated.addListener(showIcon);
 function showIcon(tabId, changeInfo, tab) {
- if (tab.url.match(/^https?:\/\/elk.bookmeter.com/) != null) {
- 	chrome.pageAction.show(tabId);
- 	console.log(changeInfo);
-  };
+  if (tab.url.match(/^https?:\/\/elk.bookmeter.com/) != null) {
+    chrome.pageAction.show(tabId);
+  }
 };
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    console.log(sender.tab ? "I got a message from a content script:" + sender.tab.url : "from the extension");
     if (request.greeting == "I am content") {
-    	var isbn = request.isbn;
-    	console.log(isbn+" in background");
-     	sendResponse({farewell: "goodbye"});
+        isbn = request.isbn;
+        console.log(isbn);
+        requestAPI(isbn);
+     	sendResponse({
+            farewell: "goodbye"
+            // library information
+        });
     } else if(request.greeting == "I am popup") {
-        var systemid_list = request.systemid_list;
-        var pref_name = request.pref_name;
+        systemid_list = request.systemid_list;
+        pref_name = request.pref_name;
         sendResponse({farewell: "bye bye"});
     }
   }
 );
+
+function requestAPI(isbn, systemid_list) {
+    var url = "https://api.calil.jp/check?appkey=02e69dccae66fb1e1c4c0b5364bbfedc&isbn="+isbn+"&systemid="+"Aomori_Pref"+"&format=json";
+    console.log(url);
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.addEventListener("load", function(r) {
+        console.log(xhr.response);
+        //calback
+    })
+    xhr.send(null);
+}
