6f1d685e496874c109de63b9d795a70340f33fbe
asterisk37n <asterisk37n@gmail.com>
2016-12-07 19:19:51 +0900

    made cross-domain connection by JSONP
    still not handling polling.

--- background.js
+++ background.js
@@ -3,25 +3,43 @@
 // found in the LICENSE file.
 
 console.log('Bckground script started');
+var app_key = "02e69dccae66fb1e1c4c0b5364bbfedc";
+var isbn;
+var systemid_list;
+var pref_name;
 chrome.tabs.onUpdated.addListener(showIcon);
 function showIcon(tabId, changeInfo, tab) {
- if (tab.url.match(/^https?:\/\/elk.bookmeter.com/) != null) {
- 	chrome.pageAction.show(tabId);
- 	console.log(changeInfo);
-  };
+  if (tab.url.match(/^https?:\/\/elk.bookmeter.com/) != null) {
+    chrome.pageAction.show(tabId);
+  }
 };
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    console.log(sender.tab ? "I got a message from a content script:" + sender.tab.url : "from the extension");
     if (request.greeting == "I am content") {
-    	var isbn = request.isbn;
-    	console.log(isbn+" in background");
-     	sendResponse({farewell: "goodbye"});
+        isbn = request.isbn;
+        console.log(isbn);
+        requestCheck(app_key,isbn,["Aomori_Pref"]);
+     	sendResponse({
+            farewell: "goodbye"
+            // put library information
+        });
     } else if(request.greeting == "I am popup") {
-        var systemid_list = request.systemid_list;
-        var pref_name = request.pref_name;
+        systemid_list = request.systemid_list;
+        pref_name = request.pref_name;
         sendResponse({farewell: "bye bye"});
     }
   }
 );
+
+function requestCheck(app_key, isbn, systemid_list) {
+    var url = "https://api.calil.jp/check?appkey="+app_key+"&isbn="+isbn+"&systemid="+systemid_list.join(",")+"&format=json&callback=callback_check";
+    console.log(url);
+    var script = document.createElement('script');
+    script.src = url;
+    document.head.appendChild(script);
+};
+
+function callback_check(json) {
+    console.log(json);
+};
