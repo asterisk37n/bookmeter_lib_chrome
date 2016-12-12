0981ddc4c3e035ff83e6ad8567f2bbf2bf45d61f
asterisk37n <asterisk37n@gmail.com>
2016-12-12 15:34:34 +0900

    showTable var changed.
    showTable var changed. background implemented requestLibrary (API) to get a library name from systemid.

--- content.js
+++ content.js
@@ -1,9 +1,7 @@
-window.onload = function() {
-  console.log("content script loaded window", document.location.toString());
-  var isbn = document.getElementsByClassName('detail__amazon')[0].getElementsByTagName('a')[0].href.split('/')[5];
-  console.log(isbn);
-  sendBackgroundIsbn(isbn);
-};
+console.log('I am content', document.location.toString());
+var isbn = document.getElementsByClassName('detail__amazon')[0].getElementsByTagName('a')[0].href.split('/')[5];
+console.log(isbn);
+sendBackgroundIsbn(isbn);
 
 var city_selector = new CalilCitySelectDlg({
 	'appkey' : '02e69dccae66fb1e1c4c0b5364bbfedc',
@@ -24,26 +22,26 @@ function on_select_city(systemid_list, pref_name){
 
 function showTable(response){
   //This is a sample
-  var response = { // this is a sample
-  	libkey: {
-		"Setagaya": "Available",
-		"Tamagawadai": "Unavailable"
-	}
-  };
+  if (response == null) {
+    response = { // this is a sample
+        "Setagaya": "Available",
+        "Tamagawadai": "Unavailable"
+    };
+  }
   var table_wrapper = document.createElement("div");
   var table = document.createElement("table");
   table_wrapper.setAttribute("class", "bm-details-side--add-border-bottom");
   table.setAttribute("class", "book-availability-table");
   table_wrapper.appendChild(table);
   
-  for(var lib in response.libkey){
+  for(var lib in response){
 	  var tr = document.createElement("tr");
 	  var th = document.createElement("th");
-	  th.setAttribute("id", response.libkey[lib] + "_label");
+	  th.setAttribute("id", response[lib] + "_label");
 	  th.innerHTML = lib;
 	  var td = document.createElement("td");
-	  td.setAttribute("headers", response.libkey[lib] + "_label");
-	  td.innerHTML = response.libkey[lib];
+	  td.setAttribute("headers", response[lib] + "_label");
+	  td.innerHTML = response[lib];
 	  tr.appendChild(th);
 	  tr.appendChild(td);
 	  table.appendChild(tr);
@@ -59,12 +57,13 @@ function sendBackgroundIsbn(isbn) {
       isbn: isbn
     }, function(response){
 	    if(response.farewell === "goodbye") {
-		    showTable(response);
+//		  showTable(response);
+          console.log("background received isbn");
 	    }else if(response.farewell === "error") {
-		    console.log("can not get library information");
+		  console.log("can not get library information");
 	    }else if(response.farewell === "initialize"){
-		    console.log("initialize");
-		    city_selector.showDlg();
+          console.log("initialize");
+          city_selector.showDlg();
 	    }
     });
 }
@@ -73,11 +72,24 @@ function sendBackgroundIsbn(isbn) {
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
-    if (request.greeting == "receiveJson"){
+    if (request.greeting == "receiveBooks"){
+      var libs_to_show ={};
       var json = request.json;
-      if(json.continue == 1) {
-        
+      for (isbn in json.books) { // just one isbn is contained
+        for (area in json.books[isbn]) {
+          var libs = json.books[isbn][area].libkey;
+          for (lib in libs) {
+            if (lib) {
+              var availability = libs[lib];
+              libs_to_show[area+lib] = availability;
+            }
+          }
+        }
       }
+      console.log(libs_to_show);
+      showTable(libs_to_show);
       sendResponse({farewell: "goodbye"}); 
+    } else if (request.greeting == "receiveLibrary") {
+      var json = request.json;
     }
   });
