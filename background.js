6f972d1005251b6e5b3f4884f2ec975ef843eefb
Shinya Onuma <ONUMAS@jp.ibm.com>
2016-12-20 15:34:56 +0900

    Completed refactoring

--- background.js
+++ background.js
@@ -4,156 +4,184 @@
 
 console.log("Bckground script started");
 
-var app_key = "02e69dccae66fb1e1c4c0b5364bbfedc";
-var isbn;
+//////////GLOBAL VARIABLES//////////
+const app_key = "02e69dccae66fb1e1c4c0b5364bbfedc";
+const baseURL = "https://api.calil.jp/";
+const MAX_REQUEST_COUNT = 5;
 var systemid_list;
 var pref_name;
-var response;
 var pref;
 var city;
+var count;
 
-chrome.storage.sync.get(["systemid_list", "pref_name"], function(result) {
-	if(!result.systemid_list || !result.pref_name)
-		return;
-  systemid_list = result.systemid_list;
-  pref_name = result.pref_name;
-  prefcity = splitPref(pref_name); //split into prefucturec and city
-  pref = prefcity[0];
-  city = prefcity[1];
-  console.log("loaded from storage");
-  console.log(systemid_list);
-  console.log(pref_name, pref+" "+city);
+//////////INITIAL PROCESS//////////
+chrome.storage.sync.get(["systemid_list", "pref_name"], function(result){
+	if(!result.systemid_list || !result.pref_name) return;
+	systemid_list = result.systemid_list;
+	pref_name = result.pref_name;
+	prefcity = splitPref(pref_name); //split into prefucturec and city
+	pref = prefcity[0];
+	city = prefcity[1];
+	console.log("loaded from storage");
+	console.log(systemid_list);
+	console.log(pref_name, pref+" "+city);
 });
 
-chrome.tabs.onUpdated.addListener(
-  function(tabId, changeInfo, tab) { // icon is visible on elk.bookmeter.com
-    if (tab.url.match(/^https?:\/\/elk.bookmeter.com/) != null) {
-      chrome.pageAction.show(tabId);
-    }
-    //if (changeInfo.status == 'complete') {
-      //requestLibrary(app_key, pref, city);
-    //}
-  }
-);
+//////////EVENT LISTENER//////////
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+	if(request.greeting === "I am content"){
+		if(!pref_name || !request.isbn){
+			sendResponse({
+				status: "ERROR",
+				message: "PARAMS_NOT_FOUND"
+			});
+			return;
+		}
+		requestCheck(app_key, request.isbn, systemid_list);
+		sendResponse({status: "SUCCESS"}); //TODO:
+	}else if(request.greeting === "I am popup"){
+	  	systemid_list = request.systemid_list;
+		pref_name = request.pref_name;
+		prefcity = splitPref(pref_name);
+		pref = prefcity[0];
+		city = prefcity[1];
+		saveChanges(systemid_list, pref_name);
+		requestLibrary(app_key, pref, city, function(response){
+			sendContentJson("receiveLibrary", response);
+		});
+	}else if(request.greeting === "LibRequest"){
+		if(app_key && pref && city){
+			requestLibrary(app_key, pref, city, function(response){
+				sendResponse({
+					status: "SUCCESS",
+					json: response
+				});
+			});
+		}else{
+			sendResponse({
+				status: "PARAMS_NOT_FOUND"
+			});
+		}
+	}
+});
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.greeting == "I am content") {
-      if (!pref_name) {
-      	sendResponse({farewell: "initialize"});
-      	return;
-      }
-      isbn = request.isbn;
-      requestCheck(app_key,isbn,systemid_list);
-      sendResponse({
-        farewell: "goodbye",
-        response: response
-      });
-    } else if(request.greeting == "I am popup") {
-      systemid_list = request.systemid_list;
-      pref_name = request.pref_name;
-      prefcity = splitPref(pref_name);
-      pref = prefcity[0];
-      city = prefcity[1];
-      saveChanges(systemid_list, pref_name);
-      requestLibrary(app_key, pref, city, function(response){
-	      sendContentJson("receiveLibrary", response);
-      });
-      sendResponse({farewell: "bye bye"});
-    }else if(request.greeting === "LibRequest"){
-	    if(app_key && pref && city){
-		    requestLibrary(app_key, pref, city, function(response){
-			    sendResponse({
-				    status: "success",
-				    json: response
-			    });
-		    });
-	   }else{
-		   sendResponse({
-			   status: "initialize"
-		   });
-	   }
-    }
- });
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){ // icon is visible on elk.bookmeter.com
+	if(tab.url.match(/^https?:\/\/elk.bookmeter.com/)){
+		chrome.pageAction.show(tabId);
+	}
+	if(changeInfo.status === "complete"){
+		sendContentJson("init", "tab updated"); //TODO: 
+	}
+});
 
-function splitPref(pref_name) {
-  var l = "都道府県".split("");
-  for (var i=0; i<l.length; i++) {
-    pre = l[i];
-    ix = pref_name.indexOf(pre);
-    if (ix > 0) {
-      pref = pref_name.substring(0,ix+1);
-      city = pref_name.substring(ix+1);
-      return [pref, city];
-    } 
-  }
+//////////XHR FUNCTIONS//////////
+
+function requestCheck(app_key, isbn, systemid_list){
+	count = 0;
+	var param = {
+		appkey: app_key,
+		isbn: isbn,
+		systemid: systemid_list.join(",")
+	};
+	baseRequest("check", param, function(response){
+		sendContentJson("receiveBooks", response);
+		if(response.continue === 1){
+			setTimeout(function(){
+				requestPolling(app_key, response.session)
+			}, 2000);
+		}
+	});
 }
 
-// send to content
-function sendContentJson(greeting, message) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {greeting: greeting,json:message}, function(response) {
-      console.log(response.farewell);
-    });
-  });
+function requestLibrary(app_key, pref, city, callback) {
+	var param = {
+		appkey: app_key,
+		pref: pref,
+		city: city
+	}
+	baseRequest("library", param, callback);
 }
 
-function requestCheck(app_key, isbn, systemid_list) {
-    var url = "https://api.calil.jp/check?appkey="+app_key+"&isbn="+isbn+"&systemid="+systemid_list.join(",")+"&format=json&callback=callbackCheck";
-    console.log(url);
-    var script = document.createElement("script");
-    script.src = url;
-    document.head.appendChild(script);
+function requestPolling(app_key, session){
+	count++;
+	if(count >= MAX_REQUEST_COUNT){
+		sendContentJson("receiveBooks", {continue: -1});
+	       	return;
+	}
+	var param = {
+		appkey: app_key,
+		session: session
+	};
+	baseRequest("check", param, function(response){
+		sendContentJson("receiveBooks", response);
+		if(response.continue === 1){
+			setTimeout(function(){
+				requestPolling(app_key, response.session)
+			}, 2000);
+		}
+	});
 }
 
-function requestPolling(app_key, session) {
-  var url = "https://api.calil.jp/check?appkey="+app_key+"&session="+session+"&format=json&callback=callbackCheck";
-  console.log(url);
-  var script = document.createElement("script");
-  script.src = url;
-  document.head.appendChild(script);
-}
-
-function requestLibrary(appkey, pref, city, callback) {
-	var URL = "https://api.calil.jp/library?appkey="+app_key+"&pref="+pref+"&city="+city+"&format=json&callback=";
+function baseRequest(type, param, callback){
+	var url = baseURL + type + "?";
+	if(param){
+		for(var i in param){
+			url += i + "=" + param[i] + "&";
+		}
+	}
+	url += "format=json&callback=";
+	if(type === "check") url += "no";
 	var xhr = new XMLHttpRequest();
-	xhr.open("GET", URL, false);
+
+	// do it synchronously to avoid to return a response to content.js before receiving the response. 
+	// Chrome warns a sync request but it's not problem because the client side process can work asynchronously.
+	xhr.open("GET", url, false); 
 	xhr.onload = function(e){
 		if(xhr.readyState === 4){
 			if(xhr.status === 200){
 				if(callback)
 					callback(JSON.parse(xhr.response));
 			}else{
+				//TODO: error handling
 			}
 		}
 	};
 	xhr.send();
 }
 
-function callbackCheck(json) {
-  console.log(json);
-  sendContentJson("receiveBooks", json);
-  if (json.continue == 1) {
-    sleep(2000);
-    requestPolling(app_key, json.session);    
-  }
+//////////UTILITY FUNCTIONS//////////
+
+function splitPref(pref_name){
+	var l = "都道府県".split("");
+	for(var i=0; i<l.length; i++){
+		pre = l[i];
+		ix = pref_name.indexOf(pre);
+		if(ix > 0){
+			pref = pref_name.substring(0,ix+1);
+			city = pref_name.substring(ix+1);
+			return [pref, city];
+		} 
+	}
 }
 
-function callbackLibrary(json) {
-  console.log(json);
-  sendContentJson("receiveLibrary", json);
+function sendContentJson(greeting, message){
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+		chrome.tabs.sendMessage(tabs[0].id, {greeting: greeting, json: message});
+	});
 }
 
-function saveChanges(systemid_list, pref_name) {
-  if (!pref_name) {
-    return;
-  }
-  chrome.storage.sync.set({"systemid_list": systemid_list, "pref_name": pref_name}, function() {
-    console.log("successfully saved: "+systemid_list+pref_name);
-  });
+function saveChanges(systemid_list, pref_name){
+	if(!pref_name) return;
+	chrome.storage.sync.set({
+		"systemid_list": systemid_list,
+		"pref_name": pref_name
+	}, function(){
+		console.log("successfully saved: "+systemid_list+pref_name);
+	});
 }
 
-function sleep(delay) {
-    var start = new Date().getTime();
-    while (new Date().getTime() - start < delay) {};
+// Deprecated: use setTimeout
+function sleep(delay){
+	var start = new Date().getTime();
+	while (new Date().getTime() - start < delay) {};
 }
